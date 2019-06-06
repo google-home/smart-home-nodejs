44589871faaca600fa9ccdbbea08f7044048e24c
Dave Smith <smithdave@google.com>
2019-06-06 11:19:09 -0600

    Update otherDeviceIds format to match frontend
    Bug: 134672432
    Signed-off-by: Dave Smith <smithdave@google.com>
    Change-Id: Ic999559a8082156794d18805df3cbc758b585deb

--- src/firestore.ts
+++ src/firestore.ts
@@ -107,7 +107,7 @@ export async function getDevices(userId: string): Promise<SmartHomeV1SyncDevices
       },
       willReportState: data.willReportState,
       attributes: data.attributes,
-      otherDeviceIds: data.otherDeviceIds.map((id: string) => ({deviceId: id})),
+      otherDeviceIds: data.otherDeviceIds,
     }
     devices.push(device)
   })
