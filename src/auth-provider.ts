cfcc2156eb8e0bd85266ff71ca2c836dbea52601
Nick Felker <fleker@google.com>
2019-10-16 15:06:15 -0400

    Add a simple authorization page for auth
    Bug: 140725857
    Change-Id: Ie52c2d307e2e6b5e265129e89d47cc5de238920a

--- src/auth-provider.ts
+++ src/auth-provider.ts
@@ -43,25 +43,33 @@ export async function getUser(headers: Headers): Promise<string> {
  * @param expressApp Express app
  */
 export async function registerAuthEndpoints(expressApp: express.Express) {
-  expressApp.use('/login', express.static('./frontend/login.html'))
+  expressApp.get('/login', (req, res) => {
+    res.send(`
+      <html>
+        <body>
+          <form action="/login" method="post">
+            <input type="hidden" name="responseurl" value="${req.query.responseurl}" />
+            <button type="submit" style="font-size:14pt">Link this service to Google</button>
+          </form>
+        </body>
+      </html>
+    `)
+  })
 
   expressApp.post('/login', async (req, res) => {
-    const {username, password} = req.body
-    console.log('/login ', username, password)
     // Here, you should validate the user account.
     // In this sample, we do not do that.
-    return res.redirect(util.format(
-      '%s?client_id=%s&redirect_uri=%s&state=%s&response_type=code',
-      '/frontend', req.body.client_id,
-      encodeURIComponent(req.body.redirect_uri), req.body.state))
+    const responseurl = decodeURIComponent(req.body.responseurl)
+    console.log(`Redirect to ${responseurl}`)
+    return res.redirect(responseurl)
   })
 
   expressApp.get('/fakeauth', async (req, res) => {
     const responseurl = util.format('%s?code=%s&state=%s',
       decodeURIComponent(req.query.redirect_uri), 'xxxxxx',
       req.query.state)
-    console.log(responseurl)
-    return res.redirect(responseurl)
+    console.log(`Set redirect as ${responseurl}`)
+    return res.redirect(`/login?responseurl=${encodeURIComponent(responseurl)}`)
   })
 
   expressApp.all('/faketoken', async (req, res) => {
